55ea7fdc282c4cc6858ad67dfc2b09391c956d39
Souvatzis Vasilios <vasouv@gmail.com>
2018-03-21 14:15:49 +0200

    changed rest controller annotations

--- src/main/java/vasouv/dietlogs/controller/MeasurementController.java
+++ src/main/java/vasouv/dietlogs/controller/MeasurementController.java
@@ -3,7 +3,6 @@ package vasouv.dietlogs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import vasouv.dietlogs.entities.Measurement;
 import vasouv.dietlogs.service.MeasurementService;
@@ -13,13 +12,13 @@ import vasouv.dietlogs.service.MeasurementService;
  * @author vasouv
  */
 @RestController
-@RequestMapping("/measurements")
+@RequestMapping("measurements")
 public class MeasurementController {
 
     @Autowired
     private MeasurementService measurementService;
 
-    @RequestMapping( value = "/all", method = RequestMethod.GET )
+    @GetMapping("all")
     public Iterable<Measurement> findAll() {
         return measurementService.findAll();
     }
--- src/main/java/vasouv/dietlogs/controller/PersonController.java
+++ src/main/java/vasouv/dietlogs/controller/PersonController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import vasouv.dietlogs.entities.Person;
@@ -15,13 +14,13 @@ import vasouv.dietlogs.service.PersonService;
  * @author vasou
  */
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("persons")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
-    @RequestMapping( value = "/all", method = RequestMethod.GET )
+    @GetMapping("all")
     public Iterable<Person> findAll() {
         return personService.findAll();
     }
