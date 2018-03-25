6397434b02fc0e7f47af64b71e9e69c9222f926d
Souvatzis Vasilios <vasouv@gmail.com>
2018-03-25 23:55:58 +0300

    added swagger

---
+++ src/main/java/vasouv/dietlogs/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package vasouv.dietlogs.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author vasouv
+ */
+@Configuration
+@EnableSwagger2 //enables swagger
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select() //returns instance of ApiSelectorBuilder, controlls endpoints
+                .apis(RequestHandlerSelectors.any()) //any documents all endpoints
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
