c3131adc22a4158564a5fdfff60eb014cbfee0ab
Souvatzis Vasilios <vasouv@gmail.com>
2018-04-18 23:45:24 +0300

    max id for saving new appointment

--- src/main/java/vasouv/dietlogs/repository/AppointmentRepository.java
+++ src/main/java/vasouv/dietlogs/repository/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package vasouv.dietlogs.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,8 @@ import vasouv.dietlogs.entities.Appointment;
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
 
     Appointment findByPersonId(int id);
+    
+    @Query("select max(a.id) from Appointment a")
+    int getMaxAppointmentID();
 
 }
