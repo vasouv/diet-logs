01ae08955f261c2b7d31aae877532706bd52baac
Souvatzis Vasilios <vasouv@gmail.com>
2018-06-08 13:22:06 +0300

    cross origin support
    instead of responses, the controller returns json

--- src/main/java/vasouv/dietlogs/controller/PersonController.java
+++ src/main/java/vasouv/dietlogs/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,33 +24,55 @@ import vasouv.dietlogs.service.PersonService;
  */
 @RestController
 @RequestMapping("persons")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
+    // @GetMapping
+    // public ResponseEntity<?> findAll() {
+    //     List<Person> all = personService.findAll();
+    //     if (all.isEmpty()) {
+    //         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    //     }
+    //     return new ResponseEntity<>(all, HttpStatus.OK);
+    // }
+
+    // @GetMapping("{id}")
+    // public ResponseEntity<Person> findById(@PathVariable int id) {
+    //     Optional<Person> found = personService.findById(id);
+    //     if (!found.isPresent()) {
+    //         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    //     }
+    //     return new ResponseEntity<>(found.get(), HttpStatus.OK);
+    // }
+
+    // @PostMapping
+    // public ResponseEntity<Person> create(@RequestBody Person person) {
+    //     Person saved = personService.create(person);
+    //     return new ResponseEntity<>(saved,HttpStatus.CREATED);
+    // }
+
+    // @DeleteMapping("/{id}")
+    // public void delete(@PathVariable int id) {
+    //     personService.delete(id);
+    // }
+
     @GetMapping
-    public ResponseEntity<?> findAll() {
-        List<Person> all = personService.findAll();
-        if (all.isEmpty()) {
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(all, HttpStatus.OK);
+    public List<Person> findAll() {
+        return personService.findAll();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Person> findById(@PathVariable int id) {
+    public Person findById(@PathVariable int id) {
         Optional<Person> found = personService.findById(id);
-        if (!found.isPresent()) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(found.get(), HttpStatus.OK);
+        return found.get();
     }
 
     @PostMapping
-    public ResponseEntity<Person> create(@RequestBody Person person) {
-        Person saved = personService.create(person);
-        return new ResponseEntity<>(saved,HttpStatus.CREATED);
+    public Person create(@RequestBody Person person) {
+        return personService.create(person);
     }
 
     @DeleteMapping("/{id}")
