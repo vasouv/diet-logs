4a529fd6e761b05f184824c09654ec9d3a53c69a
Souvatzis Vasilios <vasouv@gmail.com>
2018-06-16 23:12:36 +0300

    REST controllers return entities instead of resources
    easier to handle entities in Angular right now, will probably switch to Response again at some other point.

--- src/main/java/vasouv/dietlogs/controller/PersonController.java
+++ src/main/java/vasouv/dietlogs/controller/PersonController.java
@@ -2,12 +2,10 @@ package vasouv.dietlogs.controller;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +18,7 @@ import vasouv.dietlogs.service.PersonService;
 
 /**
  *
- * @author vasou
+ * @author vasouv
  */
 @RestController
 @RequestMapping("persons")
@@ -29,35 +27,6 @@ public class PersonController {
 
     @Autowired
     private PersonService personService;
-
-    // @GetMapping
-    // public ResponseEntity<?> findAll() {
-    //     List<Person> all = personService.findAll();
-    //     if (all.isEmpty()) {
-    //         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-    //     }
-    //     return new ResponseEntity<>(all, HttpStatus.OK);
-    // }
-
-    // @GetMapping("{id}")
-    // public ResponseEntity<Person> findById(@PathVariable int id) {
-    //     Optional<Person> found = personService.findById(id);
-    //     if (!found.isPresent()) {
-    //         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    //     }
-    //     return new ResponseEntity<>(found.get(), HttpStatus.OK);
-    // }
-
-    // @PostMapping
-    // public ResponseEntity<Person> create(@RequestBody Person person) {
-    //     Person saved = personService.create(person);
-    //     return new ResponseEntity<>(saved,HttpStatus.CREATED);
-    // }
-
-    // @DeleteMapping("/{id}")
-    // public void delete(@PathVariable int id) {
-    //     personService.delete(id);
-    // }
 
     @GetMapping
     public List<Person> findAll() {
